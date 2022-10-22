a9ab944f35d99040aabb163cd3fed9c5b6e47a8a
Marta Z Biedes <marta.tzb@gmail.com>
2022-10-22 14:15:43 -0700

    rainbows

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+
+import ColorBlock from './ColorBlock.js'
+import {ColorForm} from './ColorForm.js'
+
+function App(){
+    let [colors, setColors] = useState(['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
+    let colorMap = colors.map((color, i) => {
+      return (
+        <ColorBlock key={i} color={color} />
+      )
+    })
+
+    const addColor = (newColor) => {
+      setColors([...colors, newColor])
+    }
+
+    return (
+        <div className="App">
+          {colorMap}
+          <ColorForm addColor={addColor}/>
+        </div>
+    )
+}
+
+
+export default App;
